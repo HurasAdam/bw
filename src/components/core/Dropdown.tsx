7ddb19a2c69ba57368f3c27fa65288f50b57bc25
HurasAdam <huradam@wp.pl>
2024-09-01 14:52:11 +0200

    Dodano : Dropdown component.

---
+++ src/components/core/Dropdown.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { IoMdMore } from "react-icons/io";
+const Dropdown = ({ options = [] }) => {
+  return (
+    <div className="dropdown dropdown-end ">
+      <IoMdMore
+        tabIndex={0}
+        className="hover:text-secondary transition-all  outline-none cursor-pointer w-[21px] h-6"
+      ></IoMdMore>
+      {options.length > 0 && (
+        <ul
+          tabIndex={0}
+          className="dropdown-content cursor-pointer bg-base-100 rounded-box z-[1] w-52 p-2 shadow font-semibold "
+        >
+          {options.map(({ label, onClick, icon }) => {
+            // Sprawdzenie dla opcji
+            const isEditOption = label === "Edytuj";
+            const isDeleteOption = label === "Usuń";
+
+            // Ustawienie klasy na podstawie opcji
+            const hoverColorClass = isEditOption
+              ? "hover:bg-blue-100 transition-all"
+              : isDeleteOption
+              ? "hover:bg-rose-100 transition-all"
+              : "hover:bg-gray-200 transition-all"; // domyślny kolor dla pozostałych opcji
+
+            return (
+              <li
+                key={label} // dodaj unikalny klucz dla każdego elementu listy
+                className={`p-2 rounded ${hoverColorClass}`}
+                onClick={onClick}
+              >
+                <button className="flex items-center gap-x-2" onClick={onClick}>
+                  {icon}
+                  {label}
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Dropdown;
