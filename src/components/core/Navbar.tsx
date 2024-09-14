4f1f98816f975c5433f2eb86dc803758b94ec5d8
HurasAdam <huradam@wp.pl>
2024-09-14 22:09:43 +0200

    Dodano: ResponsiveSidebar component.

--- src/components/core/Navbar.tsx
+++ src/components/core/Navbar.tsx
@@ -42,14 +42,14 @@ const Navbar: React.FC = ({ notifications }) => {
           <GiHamburgerMenu />
         </button>
 
-        <div className="w-64 2xl:w-[350px] flex items-center py-2 px-3 gap-2 rounded-full bg-slate-100 ">
+        <div className="w-40  md:w-[250px] lg:w-[350px] flex items-center py-2 px-3 gap-2 rounded-full bg-slate-100 ">
           <MdOutlineSearch />
           <button className="w-full text-left px-1.5 hover:text-blue-200  text-gray-400 text-sm" onClick={() => setIsModalOpen(true)}>Szukaj</button>
           <SearchModal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}/>
         </div>
       </div>
 
-      <div className="flex gap-3 items-center">
+      <div className="flex gap-1 lg:gap-3 items-center">
         <NavLink
           className="bg-blue-700 hover:bg-blue-600   transition-all hover:font-bold px-2.5  py-1 rounded-full  font-semibold  text-slate-100 "
           to="articles/new-article"
